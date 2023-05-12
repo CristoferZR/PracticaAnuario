1771d56ab2430e7725b86b7f4aa9bc4d704cb5aa
Cristofer Zamora <104957113+CristoferZR@users.noreply.github.com>
2023-05-12 01:08:46 -0600

    configuring server.js to send information to the database from the registration form.

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const express = require('express')
-const mysql = mysql('mysql')
+const mysql  = require('mysql2');
 const app = express()
 const ejs = require('ejs')
-const bodyparser = require('body-parser')
+const multer = require("multer");
+const bodyParser = require('body-parser');
+
+app.use(express.json());
+
+/*--------------Variables para la conexion a la base de datos------------*/
+
+const PORT = process.env.PORT || 3000
 
  const DB_HOST = process.env.DB_HOST || 'localhost'
  const DB_USER = process.env.DB_USER || 'cris'
  const DB_PASS = process.env.DB_PASS || '1234'
- const DB_NAME = process.env.DB_NAME || 'veterinaria'
+ const DB_NAME = process.env.DB_NAME || 'anuario'
  const DB_PORT = process.env.DB_PORT || '3000'
 
 
@@ -17,8 +24,63 @@ const connection = mysql.createConnection({
     user : DB_USER,
     password : DB_PASS,
     database : DB_NAME,
-    port: DB_PORT
-
+    
 }) 
 
+connection.connect((error) => {
+    if (error) {
+      console.error('Error connecting to database:', error);
+    } else {
+      console.log('Connected to database!');
+    }
+    });
+
+
+
+/*--------------Conexion a bd------------*/
+
+app.listen(PORT);
+
 const router = express.Router();
+
+  app.set('view engine','ejs');
+  app.use(bodyParser.urlencoded({ extended: true }));
+  
+  app.use('/', router);
+
+  app.use(express.static(__dirname));
+
+
+// Configuracion de multer
+var upload = multer({
+    dest: 'public/images',
+    fileFilter : function(req, file, cb){
+        if(!file.originalname.match(/\.(jpg|jpeg)$/)){
+            return cb(new Error('Solo se permiten archivos JPG'));
+        }
+        cb(null, true);
+    }// fin del fileFilter
+})// fin del multer
+
+
+app.post("/register", upload.single('file'),(req,res)=>{
+
+    var name = req.body.nombre
+    var career = req.body.carrera
+    var image = req.file.filename
+    var email = req.body.email
+    var pass = req.body.password
+    var interests = req.body.intereses
+    var skills = req.body.habilidades
+    var objectives = req.body.objetivos
+
+    var sql = 'INSERT INTO estudiantes (nombre, carrera, imagen,email,password,intereses,habilidades,objetivos) VALUES(?,?,?,?,?,?,?,?)';
+
+    connection.query(sql,[name,career,image,email,pass,interests,skills,objectives],function(err,res){
+        if(err)throw errr
+    })
+
+    res.redirect('/index.html')
+
+})
+
